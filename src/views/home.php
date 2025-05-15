cc3b70957f0728bf0d6d3cecc8189d3012e6acb1
Robin HOTTON <robin.hotton@hotmail.com>
2025-05-15 12:14:30 +0200

    suppr: useless nav

--- src/views/home.php
+++ src/views/home.php
@@ -1,11 +1,3 @@
 <h1><?php echo $title; ?></h1>
 <p><?php echo $message; ?></p>
 <p>Votre environnement PHP MVC avec Docker fonctionne parfaitement!</p>
-
-<div style="margin-top: 20px;">
-    <h2>Navigation</h2>
-    <ul>
-        <li><a href="/users">Gestion des utilisateurs</a></li>
-        <!-- Ajoutez d'autres liens de navigation ici -->
-    </ul>
-</div>
